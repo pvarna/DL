c7741fc900978a29695e7c2a3ea557e672921825
pvarna <peter.kolev2001@gmail.com>
2025-05-01 20:09:33 +0300

    fix formatting

--- Week07-ImageSegmentation/task02.py
+++ Week07-ImageSegmentation/task02.py
@@ -5,16 +5,19 @@ from torchvision import transforms
 from torchvision.models import detection
 import matplotlib.pyplot as plt
 
+
 def get_class_names(coco_labels_path):
     with open(coco_labels_path) as file:
         class_names = [line.strip() for line in file]
     return class_names
 
+
 def main():
     model = detection.maskrcnn_resnet50_fpn(pretrained=True)
     model.eval()
 
-    image = Image.open(os.path.join('..', 'DATA', 'w07_task02.jpg')).convert('RGB')
+    image = Image.open(os.path.join('..', 'DATA',
+                                    'w07_task02.jpg')).convert('RGB')
     image_tensor = transforms.ToTensor()(image).unsqueeze(0)
 
     with torch.no_grad():
@@ -28,8 +31,12 @@ def main():
 
     fig, axs = plt.subplots(1, 2)
 
-    print(f"Object with highest confidence ({scores[0]:.4f}) is \"{class_names[labels[0] - 1]}\".")
-    print(f"Second object with highest confidence ({scores[1]:.4f}) is \"{class_names[labels[1] - 1]}\"")
+    print(
+        f"Object with highest confidence ({scores[0]:.4f}) is \"{class_names[labels[0] - 1]}\"."
+    )
+    print(
+        f"Second object with highest confidence ({scores[1]:.4f}) is \"{class_names[labels[1] - 1]}\""
+    )
 
     for i in range(2):
         axs[i].axis('off')
@@ -39,7 +46,9 @@ def main():
             cmap='jet',
             alpha=0.5,
         )
-        axs[i].set_title(f'Object: {class_names[labels[i] - 1]}') # class_names is the list with the COCO labels that you saw earlier (without enumeration)
+        axs[i].set_title(
+            f'Object: {class_names[labels[i] - 1]}'
+        )  # class_names is the list with the COCO labels that you saw earlier (without enumeration)
 
     plt.show()
 
